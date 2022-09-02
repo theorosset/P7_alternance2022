49c11ac6a5f7daf3e7234cfefc4094b394ee3925
Rosset <theo.rosset@outlook.fr>
2022-09-02 10:05:23 +0200

    refacto: add async function main, style css + img

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,22 @@
-import { Article } from "./factories/factorie.js";
-import { fetchRecipes } from "./service/service.js";
+import { Article } from './factories/factorie.js'
+import { fetchRecipes } from './service/service.js'
 import { search } from './modules/search_bar.js'
 import { displayListIngredient } from './modules/search_ingredient.js'
 import { displayListUstensil } from './modules/search_ustensils.js'
 import { displayListAppliance } from './modules/search_appliance.js'
+import { addChoiceFilter } from './modules/filterSearch.js' 
+
 
 async function main() {
-  const data = await fetchRecipes();
+  const data = await fetchRecipes()
   data.recipes.forEach((recipe) => {
-    new Article(recipe);
-  });
-  search();
-  displayListIngredient();
-  displayListUstensil();
-  displayListAppliance();
+    new Article(recipe)
+  })
+  search()
+  await displayListIngredient()
+  await displayListUstensil()
+  await displayListAppliance()
+  addChoiceFilter()
 }
 
-main();
+main()
