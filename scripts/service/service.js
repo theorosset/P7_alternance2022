586b01bdb01a53c10f06065366131558aa6da545
Rosset <theo.rosset@outlook.fr>
2022-09-09 13:15:17 +0200

    feat: init service for list of filter

--- scripts/service/service.js
+++ scripts/service/service.js
@@ -6,3 +6,40 @@ export async function fetchRecipes() {
   })
 }
 
+export async function fetchIngredientInRecipes(){
+  return await fetch("./recipes.json")
+  .then((res) => {
+   return  res.json(); 
+  }).then((data)=>{
+    let arrayIngredients = [];
+    data.recipes.forEach(recipe => recipe.ingredients.forEach(ingredient =>  arrayIngredients.push(ingredient.ingredient.toLowerCase())));
+    arrayIngredients =  [...new Set(arrayIngredients)]
+    return arrayIngredients
+  })
+}
+
+
+export async function fetchUstensilsInRecipes(){
+  return await fetch("./recipes.json")
+  .then((res) => {
+   return  res.json(); 
+  }).then((data)=>{
+    let arrayUstensils = [];
+    data.recipes.forEach(recipe => recipe.ustensils.forEach(ustensil =>  arrayUstensils.push(ustensil.toLowerCase())));
+    arrayUstensils =  [...new Set(arrayUstensils)]
+    return arrayUstensils
+  })
+}
+
+export async function fetchApplianceInRecipes(){
+  return await fetch("./recipes.json")
+  .then((res) => {
+   return  res.json(); 
+  }).then((data)=>{
+    let arrayAppliance = [];
+    data.recipes.forEach(recipe => arrayAppliance.push(recipe.appliance));
+    console.log(arrayAppliance);
+   arrayAppliance = [...new Set(arrayAppliance)]
+    return arrayAppliance
+  })
+}
