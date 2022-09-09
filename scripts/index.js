c16bf414147e087ee93ec587628250d1cd7a7b7f
Rosset <theo.rosset@outlook.fr>
2022-09-09 13:15:50 +0200

    feat: now can search by search bar

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
-import { article } from "./factories/factorie.js";
+import { Article } from "./factories/factorie.js";
 import { fetchRecipes } from "./service/service.js";
-
+import { search } from "./modules/search_bar.js"
 
 async function main (){
     const data = await fetchRecipes()
-    
     data.recipes.forEach(recipe => {
-        new article(recipe)
+        new Article(recipe)
     });
-    
+    search()
 }
 main()
---
+++ scripts/modules/search_bar.js
@@ -0,0 +1,21 @@
+export function search() {
+  const h2All = Array.from(document.querySelectorAll('.recipe-title-time h2'))
+  const articles = document.querySelectorAll('.recipe')
+  const input = document.querySelector('.search_bar')
+
+  console.log(articles)
+  input.addEventListener('keyup', (e) => {
+    const string = e.target.value.toLowerCase()
+    h2All.filter((h2) => {
+      if(!h2.innerText.toLowerCase().includes(string)){
+        const parent = h2.closest('article')
+        parent.remove()
+      } else {
+        const section = document.querySelector("#section_recipes")
+        const parent = h2.closest('article')
+        section.appendChild(parent)
+        
+      }
+    })
+  })
+}
