95d3e2fb37ddcd58e8dabc11e5079e7956b1d420
Rosset <theo.rosset@outlook.fr>
2022-09-29 19:15:11 +0200

    fix: now you can search with filter

--- scripts/modules/searchByFilter.js
+++ scripts/modules/searchByFilter.js
@@ -2,35 +2,45 @@
 export function addFilter() {
   const allLi = document.querySelectorAll('.list li')
 
+  let elements = { articles: [], ingredients: [], "list-red": [],  "list-green": [],  "list-blue": [] };
+  
   allLi.forEach((li) => {
     li.addEventListener('click', (event) => {
       if (event.target.closest('ul')) {
         const parentClass = event.target.closest('ul').classList.value
+        const parentClassPart = parentClass.split(" ")
+        
         const newLi = document.createElement('li')
-        newLi.innerHTML = `${li.innerText} <img src='./assets/cross.svg' alt='supprimer le choix'>`
+        newLi.innerHTML = `${li.innerText} <img class="cross" src='./assets/cross.svg' alt='supprimer le choix'>`
         newLi.classList.add('choice')
 
-        if (parentClass === 'list list-blue') {
-          addClassOfChoice('list-blue', newLi)
-          searchInIngredient(newLi)
+        if(parentClassPart[0] === "list" && parentClassPart[1].indexOf("list-") > -1) {
+
+          addClassOfChoice(parentClassPart[1], newLi)
+
+          if (parentClassPart[1].indexOf('blue') > -1) {
+            elements = { ...elements, ...searchInIngredient(parentClassPart[1]) }
+          } else {
+            const dataName = parentClassPart[1] === "list-red" ? 'data-ustensil' : 'data-appliance';
+            elements = { ...elements, ...searchInApplianceOrUstensil(dataName, parentClassPart[1]) }            
+          }
+          
+          //elements.choices = [...elements["list-red"] ?? [], ...elements["list-green"] ?? [] ,...elements["list-blue"] ?? []]
+          li.classList.add("displayNone")
         }
-        if (parentClass === 'list list-red') {
-          addClassOfChoice('list-red', newLi)
-          searchInApplianceOrUstensil('data-ustensil', 'list-red', newLi)
-        }
-        if (parentClass === 'list list-green') {
-          addClassOfChoice('list-green', newLi)
-          searchInApplianceOrUstensil('data-appliance', 'list-green', newLi)
-        }
+   
+        newLi.querySelector('.cross').addEventListener('click', (event) => deleteFilter(event, elements, li, parentClassPart[1]));
       }
     })
   })
 }
 
+
 /**
  * 
  * @param {HTMLClassElement} classList 
  * @param {HTMLElement} li 
+ * 
  */
 function addClassOfChoice(classList, li) {
   const filterChoice = document.querySelector('.filterChoice')
@@ -39,66 +49,56 @@ function addClassOfChoice(classList, li) {
 }
 
 //------------ingredient search------------------
-function searchInIngredient(li) {
+function searchInIngredient(classList, doNotFilter) {
+
   const ingredients = Array.from(document.querySelectorAll('.ingredient'))
+    .filter((ingredient) => doNotFilter || !ingredient.closest('article').classList.contains('displayNone'))
+
   const choices = Array.from(document.querySelectorAll('.choice.list-blue'))
 
-  ingredients.forEach((ingredient) => {
+  ingredients.forEach((ingredient) => {    
     choices.forEach((choice) => {
-      ingredientMatch(choice, ingredient)
+      textMatch(choice, ingredient)
     })
   })
 
-  deletFilter(li, choices, ingredients)
+  const elements = { ingredients };
+  elements[classList] = choices;
+
+  return elements;
 }
 
-/**
- * function Look if choice match with ingredient
- *
- * @param {HTMLElement} choice its filter choice
- * @param {HTMLElement} ingredient its ingredient of recipes
- */
-function ingredientMatch(choice, ingredient) {
-  const section = document.querySelector('#section_recipes')
-  const regexp = new RegExp(choice.innerText, 'gi')
-  if (ingredient.innerText.match(regexp)) {
-    section.appendChild(ingredient.closest('article'))
+function textMatch(choice, htmlElementOrAttribute, dataAttribute) {
+  const regexp = new RegExp(choice.innerText.toLowerCase(), 'gi')
+  let value;
+
+  if (htmlElementOrAttribute.tagName === 'ARTICLE' && dataAttribute) {
+    value = htmlElementOrAttribute.getAttribute(dataAttribute).toLowerCase() 
+    htmlElementOrAttribute.classList.toggle('displayNone', !regexp.test(value))
   } else {
-    ingredient.closest('article').remove()
+    value = htmlElementOrAttribute.innerText.toLowerCase()
+    htmlElementOrAttribute.closest('article').classList.toggle('displayNone', !regexp.test(value))
   }
+
 }
 
-//----------------search in ustensil or appliance------------
-/**
- * this function look if appliance or ustensil match with choice
- *
- * @param {HTMLElement} choice
- * @param {HTMLDataElement} attributeOfArticle
- * @param {HTMLElement} article
- *
- */
-function ustensilApplianceMatch(choice, attributeOfArticle, article) {
-  const section = document.querySelector('#section_recipes')
-  const regexp = new RegExp(choice.innerText, 'gi')
-  if (attributeOfArticle.match(regexp)) {
-    section.appendChild(article)
-  } else {
-    article.remove()
-  }
-}
-
-function searchInApplianceOrUstensil(dataSet, classList, li) {
-  const articles = document.querySelectorAll('.recipe')
+function searchInApplianceOrUstensil(dataSet, classList, doNotFilter) {
+  const articles = Array.from(document.querySelectorAll('.recipe')).filter(
+    (article) => doNotFilter || !article.classList.contains('displayNone')
+  )
+  console.log(classList);
   const choices = Array.from(document.querySelectorAll(`.choice.${classList}`))
 
   choices.forEach((choice) => { 
     articles.forEach((article) => {
-      const applianceOfArticle = article.getAttribute(dataSet).toLowerCase()
-      ustensilApplianceMatch(choice, applianceOfArticle, article)
+      textMatch(choice, article, dataSet)
     })
   })
 
-  deletFilter(li, choices, articles)
+  const elements = { articles };
+  elements[classList] = choices;
+
+  return elements;
 }
 
 
@@ -109,25 +109,36 @@ function searchInApplianceOrUstensil(dataSet, classList, li) {
  * @param {HTMLBaseElement} ingredientsOrArticles 
  */
 
-function deletFilter(li, choices, ingredientsOrArticles) {
-  const section = document.querySelector('#section_recipes')
+function deleteFilter(event, elements, liInlistOfFilter, classList ) {
+  let dataName;
+
+  if (classList.indexOf('blue') === -1) {
+    dataName = classList === "list-red" ? 'data-ustensil' : 'data-appliance';
+  }
+
+  const liInFilterChoice = event.target.closest('li')
+ 
+  const indexChoice = elements[classList].findIndex((choice) => choice.innerText === liInFilterChoice.innerText)
+
+  elements[classList].splice(indexChoice, 1)
+  let choices = [...elements["list-red"] ?? [], ...elements["list-green"] ?? [] ,...elements["list-blue"] ?? []]
   
-  li.children[0].addEventListener('click', () => {
-    const indexChoice = choices.findIndex((choice) => { return choice.innerText === li.innerText})
-    li.remove()
-    choices.splice(indexChoice, 1)
-
-    if (choices.length === 0) {
-      ingredientsOrArticles.forEach((ingredient) => {
-        section.appendChild(ingredient.closest('article'))
-      })
-      console.log('ok')
-    }
-
-    choices.forEach((choice) => {
-      ingredientsOrArticles.forEach((ingredient) => {
-        ingredientMatch(choice, ingredient)
-      })
-    })
-  })
+  console.log(elements);
+  liInFilterChoice.remove()
+  liInlistOfFilter.classList.remove("displayNone")
+  
+  if (choices.length === 0) {
+    document.querySelectorAll('article').forEach((article)=>article.classList.toggle('displayNone', false));
+  } else {
+    searchInApplianceOrUstensil('data-ustensil', 'list-red', true)
+    searchInApplianceOrUstensil('data-appliance', 'list-green', true)
+    searchInIngredient('list-blue', true)
+    // elements.choices.forEach((choice) => {
+    //   document.querySelectorAll('article').forEach((article) => {
+    //     textMatch(choice, article, 'data-ustensil')
+    //     textMatch(choice, article, 'data-appliance')
+    //     textMatch(choice, article)
+    //   });
+    // });
+  }
 }
