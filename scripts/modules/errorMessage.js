38f5d3d4df99af6e139a82d9fe9bac117bb241d0
Rosset <theo.rosset@outlook.fr>
2022-10-12 12:21:08 +0200

    refacto(errorMessage): delet useless line

--- scripts/modules/errorMessage.js
+++ scripts/modules/errorMessage.js
@@ -8,17 +8,10 @@ export function errorMessageIfSearchWithBar(section) {
       }
 }
 
-export function errorMessageIfSearchWithFilter(section){
+export function errorMessageIfSearchWithFilter(){
   const errorMessage = document.querySelector(".errorMessage");
-  let numberOfElementHaveDisplayNone = 0;
-    for (let i = 0; i < section.children.length; i++) {
-        const article = section.children[i];
-        if (article.classList.contains("displayNone")) { 
-            numberOfElementHaveDisplayNone++;
-          }
-    }
-  
-  if(section.children.length === numberOfElementHaveDisplayNone) {
+
+  if(document.querySelectorAll(".recipe").length === document.querySelectorAll(".recipe.displayNone").length) {
     errorMessage.classList.remove("displayNone");
   } else {
     errorMessage.classList.add("displayNone");
