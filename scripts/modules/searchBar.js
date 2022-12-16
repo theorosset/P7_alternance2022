9184d4d9bb4a56a11431ee641676d2423d785a0b
Rosset <theo.rosset@outlook.fr>
2022-12-16 16:24:01 +0100

    refacto: review

--- scripts/modules/searchBar.js
+++ scripts/modules/searchBar.js
@@ -6,47 +6,84 @@ import { getRecipesMatch } from "./searchByFilter.js";
  * @param {string} searchValue
  */
 export function updateRecipes(searchValue) {
-	const articles = Array.from(document.querySelectorAll(".recipe"));
 	let choices = Array.from(document.querySelectorAll(".choice"));
+	let articles = Array.from(document.querySelectorAll(".recipe"));
 	let articlesWithoutDisplayNone = [];
 
-	//one time check choice
+	for(let i = 0; i < articles.length; i++) {
+		const article = articles[i];
+		article.classList.toggle("displayNone", false);
+	}
+	
 	if (choices.length === 0) {
 		for(let i = 0; i < articles.length; i++) {
 			const article = articles[i];
 			article.classList.toggle("displayNone", false);
 		}
 	} else {
-		for(let i = 0; i < choices.length; i++) {
-			const choice = choices[i];
+		updateRecipesByFilter(choices, articles);
+	}
+
+	for(let i = 0; i < articles.length; i++) {
+		const article = articles[i];
+		if (!article.classList.contains("displayNone")) {
+			articlesWithoutDisplayNone.push(article);
+		}
+	}
+
+	if (String(searchValue).trim() !== "") {
+		for(let i = 0; i < [searchValue].length; i++) {
+			const choice = [searchValue][i];
 			for(let i = 0; i < articles.length; i++) {
 				const article = articles[i];
 				getRecipesMatch(choice, article);
 			}
 		}
-	}
-	for(let i = 0; i < articles.length; i++) {
-		const article = articles[i];
-		if(!article.classList.contains("displayNone")) {
-			articlesWithoutDisplayNone.push(article);
+
+		articles = Array.from(document.querySelectorAll(".recipe"))
+			.filter((article) => !article.classList.contains("displayNone"));
+
+		if (choices.length === 0) {
+			for(let i = 0; i < articles.length; i++) {
+				const article = articles[i];
+				article.classList.toggle("displayNone", false);
+			}
+		} else {
+			updateRecipesByFilter(choices, articles);
 		}
 	}
-	//two time check search bar value
-	if (String(searchValue).trim() === "undefined" || String(searchValue).trim() === "" ) {
-		articlesWithoutDisplayNone = [];
-		for(let i = 0; i < articles.length; i++) {
+}
+
+/**
+ * this function return data-attribute use for search
+ * @param { Object } choice 
+ * @returns { String } return data attribute
+ */
+function getDataName(choice) {
+	let dataAttribute = "";
+	if (typeof choice === "object") {
+		if(choice.classList.contains("list-red") || choice.classList.contains("list-green")) {
+			dataAttribute = choice.classList.contains("list-red") ? "data-ustensil" : "data-appliance";
+		}
+	}
+	return dataAttribute;
+}
+
+//update
+function updateRecipesByFilter(choices, articles) {
+	let articlesWithoutDisplayNone = [];
+
+	for (let i = 0; i < choices.length; i++) {
+		const choice = choices[i];
+		for (let i = 0; i < articles.length; i++) {
 			const article = articles[i];
-			if(!article.classList.contains("displayNone")) {
+			if (!article.classList.contains("displayNone")) {
 				articlesWithoutDisplayNone.push(article);
 			}
 		}
-	} else {
-		for (let i = 0; i < [searchValue].length; i++) {
-			const choice = [searchValue][i];
-			for (let i = 0; i < articlesWithoutDisplayNone.length; i++ ) {
-				const article = articlesWithoutDisplayNone[i];
-				getRecipesMatch(choice, article);
-			}
+		for(let i = 0; i < articlesWithoutDisplayNone.length; i++) {
+			const articleWithoutDisplayNone =  articlesWithoutDisplayNone[i];
+			getRecipesMatch(choice, articleWithoutDisplayNone, getDataName(choice));
 		}
 	}
 }
@@ -62,7 +99,7 @@ export function search() {
 		if (searchValue.length > 2) {
 			updateRecipes(searchValue);
 		} else if (searchValue.length === 0) {
-			updateRecipes();
+			updateRecipes("");
 		}
 		displayErrorMessage();
 	});
